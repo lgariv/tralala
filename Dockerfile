bd592d90bdcf4312d857a6d59eb8b16a8577b240
lgariv <lavie43b@gmail.com>
2023-05-27 18:05:09 +0300

    added Dockerfile and summary

---
+++ Dockerfile
@@ -0,0 +1,56 @@
+# Install dependencies only when needed
+FROM node:19.5.0-alpine3.17 AS deps
+
+# Check https://github.com/nodejs/docker-node/tree/b4117f9333da4138b03a546ec926ef50a31506c3#nodealpine to understand why libc6-compat might be needed.
+RUN apk add --update --no-cache libc6-compat
+WORKDIR /app
+
+# Install dependencies based on the preferred package manager
+COPY package.json yarn.lock* package-lock.json* pnpm-lock.yaml* ./
+RUN \
+  if [ -f yarn.lock ]; then yarn --frozen-lockfile; \
+  elif [ -f package-lock.json ]; then npm ci; \
+  elif [ -f pnpm-lock.yaml ]; then yarn global add pnpm && pnpm i --frozen-lockfile; \
+  else echo "Lockfile not found." && exit 1; \
+  fi
+
+# Rebuild the source code only when needed
+FROM node:19.5.0-alpine3.17 AS builder
+WORKDIR /app
+COPY --from=deps /app/node_modules ./node_modules
+COPY . .
+
+ENV NEXT_TELEMETRY_DISABLED 1
+
+RUN npm run build
+
+# pull official base image
+FROM node:19.5.0-alpine3.17 AS runner
+
+# set working directory
+WORKDIR /app
+
+RUN apk add --update --no-cache tzdata
+
+# fix time zone
+RUN cp /usr/share/zoneinfo/Israel /etc/localtime
+RUN echo "Israel" > /etc/timezone
+ENV TZ Israel
+
+ENV NODE_ENV production
+
+ENV NEXT_TELEMETRY_DISABLED 1
+
+COPY --from=builder /app/public ./public
+
+# Automatically leverage output traces to reduce image size
+# https://nextjs.org/docs/advanced-features/output-file-tracing
+COPY --from=builder /app/.next/standalone ./
+COPY --from=builder /app/.next/static ./.next/static
+
+EXPOSE 3000
+
+ENV PORT 3000
+
+# start app
+CMD ["sh", "-c", "node server.js"]
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,12 +4,32 @@ import Image from "next/image";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
 import { useBoardStore } from "@/store/BoardStore";
+import { useEffect, useState } from "react";
 
 function Header() {
-	const [searchString, setSearchString] = useBoardStore((state) => [
+	const [board, searchString, setSearchString] = useBoardStore((state) => [
+		state.board,
 		state.searchString,
 		state.setSearchString,
 	]);
+
+	const [todosCount, setTodosCount] = useState(0);
+	const [inProgressCount, setInProgressCount] = useState(0);
+	const [doneCount, setDoneCount] = useState(0);
+	const [loading, setLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		if (board.columns.size === 0) {
+			setLoading(true);
+			return;
+		}
+		const entries = Array.from(board.columns.entries());
+		setTodosCount(entries[0][1]["todos"].length);
+		setInProgressCount(entries[1][1]["todos"].length);
+		setDoneCount(entries[2][1]["todos"].length);
+		setLoading(false);
+	}, [board]);
+	
 
 	return (
 		<header>
@@ -31,7 +51,7 @@ function Header() {
 						<input
 							type="text"
 							placeholder="Search"
-							onChange={(e) => setSearchString (e.target.value)}
+							onChange={(e) => setSearchString(e.target.value)}
 							className="flex-1 outline-none p-2"
 						/>
 						<button></button>
@@ -44,8 +64,14 @@ function Header() {
 
 			<div className="flex items-center justify-center px-5 py-2 md:py-5">
 				<p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
-					<UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
-					GPT is summarising your tasks for this day...
+					<UserCircleIcon
+						className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${loading && "animate-spin"}`}
+					/>
+					{loading
+					? "Summarising you tasks for the day..."
+					: `Welcome Zira! Welcome to the tralala app! Here's a summary
+					of your to-dos: You have ${todosCount} to-do, ${inProgressCount} in progress, and ${doneCount} done
+					task. Have a productive day!`}
 				</p>
 			</div>
 		</header>
