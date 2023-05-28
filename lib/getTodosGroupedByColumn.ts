7f8d481cb8bcd3bac9e45afbc07e0fac6134eda3
lgariv <lavie43b@gmail.com>
2023-05-28 20:06:06 +0300

    test

--- lib/getTodosGroupedByColumn.ts
+++ lib/getTodosGroupedByColumn.ts
@@ -9,48 +9,52 @@ export const getTodosGroupedByColumn = async () => {
     
     console.log(`todos :: ${JSON.stringify(todos)}`);
     
-    const columns = todos.reduce((acc, todo) => {
-        if (!acc.get(todo.status)) {
-            acc.set(todo.status, {
-                id: todo.status,
-                todos: [],
-            })
-        }
+    // const columns = todos.reduce((acc, todo) => {
+    //     if (!acc.get(todo.status)) {
+    //         acc.set(todo.status, {
+    //             id: todo.status,
+    //             todos: [],
+    //         })
+    //     }
 
-        acc.get(todo.status)!.todos.push({
-			id: todo.id,
-			createdAt: todo.createdAt,
-			title: todo.title,
-            status: todo.status,
-            ...(todo.image && {image: JSON.parse(todo.image)})
-        });
+    //     acc.get(todo.status)!.todos.push({
+	// 		id: todo.id,
+	// 		createdAt: todo.createdAt,
+	// 		title: todo.title,
+    //         status: todo.status,
+    //         ...(todo.image && {image: JSON.parse(todo.image)})
+    //     });
         
-        return acc;
+    //     return acc;
 
-    }, new Map<TypedColumn, Column>)
+    // }, new Map<TypedColumn, Column>)
 
-    // if columns doesnt have inprogress, todo and done, add them with empty todos
-    const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
-    for (const columnType of columnTypes) {
-        if (!columns.get(columnType)) {
-            columns.set(columnType, {
-                id: columnType,
-                todos: [],
-            })
-        }
-    }
+    // // if columns doesnt have inprogress, todo and done, add them with empty todos
+    // const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
+    // for (const columnType of columnTypes) {
+    //     if (!columns.get(columnType)) {
+    //         columns.set(columnType, {
+    //             id: columnType,
+    //             todos: [],
+    //         })
+    //     }
+    // }
     
-    // sort columns by column types
-    const sortedColumns = new Map(
-		Array.from(columns.entries()).sort(
-			(a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
-		)
-    );
+    // // sort columns by column types
+    // const sortedColumns = new Map(
+	// 	Array.from(columns.entries()).sort(
+	// 		(a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+	// 	)
+    // );
     
-    console.log(`sortedColumns :: ${JSON.stringify(sortedColumns)}`);
+    // console.log(`sortedColumns :: ${JSON.stringify(sortedColumns)}`);
+
+    // const board: Board = {
+    //     columns: sortedColumns,
+    // }
 
     const board: Board = {
-        columns: sortedColumns,
+        columns: {},
     }
 
     return board;
