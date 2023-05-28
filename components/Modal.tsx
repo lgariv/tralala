3b2c2f2289b19b116a829da54ec4fa47620387f1
lgariv <lavie43b@gmail.com>
2023-05-29 01:49:13 +0300

    prevent click on Add Task if no performer input

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -109,7 +109,7 @@ function Modal() {
 								<div className="pt-4">
 									<button
 										type="submit"
-										disabled={!newTaskInput}
+										disabled={!newTaskInput || !newTaskPerformerInput}
 										className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:bg-gray-100 disabled:text-gray-300 disabled:cursor-not-allowed "
 									>
 										Add Task
