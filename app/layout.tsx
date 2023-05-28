060ac3b6e9401fd5dc137fb9afa1f846581bec45
lgariv <lavie43b@gmail.com>
2023-05-28 22:27:29 +0300

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="shortcut icon" href="/favicon.ico" />
+			</head>
 			<body className="bg-[#F5F6F8]">
 				{children}
 				<Modal />
